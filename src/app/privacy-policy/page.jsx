6b37a7fed37ae7d1b305e4b1a58017baf6cf3361
upturnist <shamnad2nk@gmail.com>
2025-12-22 17:18:42 +0530

    first commit

--- src/app/privacy-policy/page.jsx
+++ src/app/privacy-policy/page.jsx
@@ -3,14 +3,14 @@ import { privacyHtml } from "@/utils/legalContent";
 
 const PrivacyPolicyPage = () => {
   return (
-    <main className="bg-[#2A6F7E] min-h-screen py-12 md:py-16 px-4 md:px-15">
-      <div className="max-w-4xl mx-auto bg-white/5 rounded-3xl px-6 md:px-10 py-8 md:py-10 text-white">
+    <main className=" min-h-screen pt-30 pb-20 md:pt-56 md:pb-20 px-4 md:px-15">
+      <div className="max-w-4xl mx-auto bg-white/5 rounded-3xl px-6 md:px-10 pt-8 md:py-10 text-white">
         <h1 className="apollo-heading text-2xl md:text-[28px] mb-6">
           Privacy Policy
         </h1>
 
         <div
-          className="prose prose-invert max-w-none text-sm font-extralight leading-relaxed"
+          className="prose prose-invert max-w-none text-sm font-extralight leading-relaxed pt-10"
           dangerouslySetInnerHTML={{ __html: privacyHtml }}
         />
       </div>
--- src/app/terms-conditions/page.jsx
+++ src/app/terms-conditions/page.jsx
@@ -3,7 +3,7 @@ import { termsHtml } from "@/utils/legalContent";
 
 const TermsPage = () => {
   return (
-    <main className="bg-[#2A6F7E] min-h-screen py-12 md:py-16 px-4 md:px-15">
+    <main className=" min-h-screen pt-30 pb-20 md:pt-56 md:pb-20 px-4 md:px-15">
       <div className="max-w-4xl mx-auto bg-white/5 rounded-3xl px-6 md:px-10 py-8 md:py-10 text-white">
         <h1 className="apollo-heading text-2xl md:text-[28px] mb-6">
           Terms &amp; Conditions
