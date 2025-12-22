cfc13a5b322403e6925cfedee73d7ca5178d1b15
upturnist <shamnad2nk@gmail.com>
2025-12-22 14:30:08 +0530

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -83,7 +83,6 @@ export default function RootLayout({ children }) {
           }}
         />
         {children}
-        {/* <CtaForm /> */}
         <CtaWrapper/>
         <Footer />
       </body>
