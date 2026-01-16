57ca613b4fe15935bf2abc1a34d4451da338c272
shamnad-sd <shamnad2nk@gmail.com>
2026-01-16 10:36:56 +0530

    favicon

--- src/lib/generateMetadata.js
+++ src/lib/generateMetadata.js
@@ -16,8 +16,8 @@ export function generateMetadata(seoData, nofollow, path) {
     icons: {
       icon: [
     // { url: '/favicon.ico' }, // Google reads this
-    { url: '/favicon.png', sizes: '32x32', type: 'image/png' },
-    { url: '/favicon.png', sizes: '16x16', type: 'image/png' },
+    { url: 'https://www.shantirarealty.com/favicon.png', sizes: '32x32', type: 'image/png' },
+    { url: 'https://www.shantirarealty.com/favicon.png', sizes: '16x16', type: 'image/png' },
   ],
       apple: [
         { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
